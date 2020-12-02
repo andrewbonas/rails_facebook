aec70097b48c91c0208dffd90339405e8af56a45
Andrew Bonas <aabonas@hotmail.com>
2020-12-02 13:04:54 -0800

    Assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.require_master_key = true 
+  config.action_controller.relative_url_root = "/myapp"
 end
